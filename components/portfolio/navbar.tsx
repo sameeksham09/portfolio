4bc9cb295f6c5cae357230614ade21de051278fc
Sameeksha M <94727258+sameeksham09@users.noreply.github.com>
2026-05-20 19:51:08 -0700

    Update navbar.tsx

--- components/portfolio/navbar.tsx
+++ components/portfolio/navbar.tsx
@@ -14,7 +14,7 @@ const navLinks = [
   { label: "Projects", href: "#projects" },
   { label: "Education", href: "#education" },
   { label: "Publications", href: "#publications" },
-  { label: "Resume", href: "#resume" },
+  { label: "Resume", href: "/Sameeksha-Mahesh-Resume.pdf" },
   { label: "Contact", href: "#contact" },
 ];
 
